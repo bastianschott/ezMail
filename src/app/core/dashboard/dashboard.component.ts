78f263c6e0de55fb65c07283b8aae15ff7da8d3c
Bastian Schott <b.sch94@gmail.com>
2019-10-23 10:07:05 +0200

    added trackBy

--- src/app/core/dashboard/dashboard.component.ts
+++ src/app/core/dashboard/dashboard.component.ts
@@ -13,21 +13,29 @@ export class DashboardComponent {
     map(({ matches }) => {
       if (matches) {
         return [
-          { title: 'Card 1', cols: 1, rows: 1 },
-          { title: 'Card 2', cols: 1, rows: 1 },
-          { title: 'Card 3', cols: 1, rows: 1 },
-          { title: 'Card 4', cols: 1, rows: 1 },
+          { title: 'Card 1', id: 1, cols: 1, rows: 1 },
+          { title: 'Card 2', id: 2, cols: 1, rows: 1 },
+          { title: 'Card 3', id: 3, cols: 1, rows: 1 },
+          { title: 'Card 4', id: 4, cols: 1, rows: 1 },
         ];
       }
 
       return [
-        { title: 'Card 1', cols: 2, rows: 1 },
-        { title: 'Card 2', cols: 1, rows: 1 },
-        { title: 'Card 3', cols: 1, rows: 2 },
-        { title: 'Card 4', cols: 1, rows: 1 },
+        { title: 'Card 1', id: 1, cols: 2, rows: 1 },
+        { title: 'Card 2', id: 2, cols: 1, rows: 1 },
+        { title: 'Card 3', id: 3, cols: 1, rows: 2 },
+        { title: 'Card 4', id: 4, cols: 1, rows: 1 },
       ];
     })
   );
 
   constructor(private breakpointObserver: BreakpointObserver) {}
+
+  // https://medium.com/better-programming/improving-angular-ngfor-performance-through-trackby-ae4cf943b878
+  trackByFunction(item: any) {
+    if (!item) {
+      return null;
+    }
+    return item.id;
+  }
 }
