761f2d3659ebae71dcff0da8f6f5a1cd326617fa
Benedikt Strozynski <strozynskib@yahoo.de>
2019-10-25 12:59:11 +0200

    Versuch die Weiterleitung nach Login zu managen....

--- src/app/core/mail-settings/mail-settings.component.ts
+++ src/app/core/mail-settings/mail-settings.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { AuthGuard } from 'src/app/shared/auth.guard';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-mail-settings',
@@ -7,8 +9,12 @@ import { Title } from '@angular/platform-browser';
   styleUrls: ['./mail-settings.component.scss'],
 })
 export class MailSettingsComponent implements OnInit {
-  constructor(private titleService: Title) {
+  constructor(private titleService: Title, private authGuard: AuthGuard, private router: Router) {
     this.titleService.setTitle('Mail Einstellungen | ezMail');
+
+    if (this.authGuard.canActivate()) {
+      this.router.navigate([{ outlets: { primary: ['login'], toolbar: ['login'] } }]);
+    }
   }
 
   ngOnInit() {}
--- src/app/menu/sidenav/sidenav.component.ts
+++ src/app/menu/sidenav/sidenav.component.ts
@@ -31,7 +31,6 @@ export class SidenavComponent implements OnInit {
   }
 
   closeSideNav() {
-    console.log('Drawer mode: ' + this.drawer.mode);
     if (this.drawer.mode === 'over') {
       this.drawer.close();
     }
