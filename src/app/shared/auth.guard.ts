56218aad71ff879a8e5b8a47b1a68ed580542b99
Bastian Schott <b.sch94@gmail.com>
2019-11-07 14:36:21 +0100

    installed hammer.JS as dependency

--- src/app/shared/auth.guard.ts
+++ src/app/shared/auth.guard.ts
@@ -1,7 +1,7 @@
 import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { AuthenticationService } from './authentication.service';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { CanActivate, Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
@@ -9,20 +9,7 @@ import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from
 export class AuthGuard implements CanActivate {
   constructor(private authService: AuthenticationService, private router: Router) {}
 
-  canActivate() {
+  canActivate(): Observable<boolean> {
     return this.authService.getUserIsLoggedIn$();
   }
-
-  /*
-  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
-    return this.authentication.user.pipe(
-      take(1),
-      map(user => !!user),
-      tap(loggedIn => {
-        if (!loggedIn) {
-          this.router.navigate(['/login']);
-        }
-      })
-    );
-  }*/
 }
