b9be89f42478a218f23dd2f358790fb32025b521
Benedikt Strozynski <strozynskib@yahoo.de>
2019-11-09 11:26:34 +0100

    mail-settings weiterleitung funktioniert wieder

--- src/app/core/mail-settings/mail-settings.component.ts
+++ src/app/core/mail-settings/mail-settings.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { AuthGuard } from 'src/app/shared/auth.guard';
 import { Router } from '@angular/router';
+import { AuthenticationService } from 'src/app/shared/authentication.service';
 
 @Component({
   selector: 'app-mail-settings',
@@ -9,13 +10,14 @@ import { Router } from '@angular/router';
   styleUrls: ['./mail-settings.component.scss'],
 })
 export class MailSettingsComponent implements OnInit {
-  constructor(private titleService: Title, private authGuard: AuthGuard, private router: Router) {
+  constructor(private titleService: Title, private authService: AuthenticationService, private router: Router) {}
+
+  ngOnInit() {
+    this.authService.setRoutingDestination();
     this.titleService.setTitle('Mail Einstellungen | ezMail');
 
-    if (this.authGuard.canActivate()) {
-      this.router.navigate([{ outlets: { primary: ['login'], toolbar: ['login'] } }]);
-    }
+    //  if (this.authGuard.canActivate()) {
+    //    this.router.navigate([{ outlets: { primary: ['login'], toolbar: ['login'] } }]);
+    //  }
   }
-
-  ngOnInit() {}
 }
